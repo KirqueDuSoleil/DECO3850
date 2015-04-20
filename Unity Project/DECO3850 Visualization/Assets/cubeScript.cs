14d601886eba20c3363c848b9107354abfd72bf2
KirqueDuSoleil <kirk@radunz.com.au>
2015-04-20 14:41:47 +1000

    First Commit

---
+++ Unity Project/DECO3850 Visualization/Assets/cubeScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Changing the colour of the cube so that we can see it
+		gameObject.GetComponent<Renderer>().material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Handling moving the cube with the arrow keys
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z--;
+			this.transform.position = position;
+		}
+	}
+}
